be3c959ce0213426846f01b0a24ad3b0ab43b75a
Delibee-hkust <akksown@gmail.com>
2025-02-20 02:10:22 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const API_KEY = 'AIzaSyCneyK_cu8HByPETu7LhviiedMoJHQi21k'; // 여기에 본인의 API 키를 입력하세요.
 const SPREADSHEET_ID = '1IobwmmI-zhvliF1y-_mlmYu0n19JSRsp6NMpQlyz-gM'; // 구글 시트의 ID를 입력하세요.
-const RANGE = 'Inventory!B2:B8'; // 데이터를 가져올 범위를 설정합니다.
+const RANGE = 'Inventory!A1:F8'; // 데이터를 가져올 범위를 설정합니다.
 
 function getMenuData() {
     const url = `https://sheets.googleapis.com/v4/spreadsheets/${SPREADSHEET_ID}/values/${RANGE}?key=${API_KEY}`;
